f999fa827bffb1a003d3b624b03413a73f7f789c
MCSeal <mzeal93@gmail.com>
2022-12-06 18:24:25 -0500

    types

--- index.ts
+++ index.ts
@@ -1,9 +1,25 @@
 import axios from 'axios';
-
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then((response) => {
-  console.log(response.data);
-  console.log('success');
-  console.log('success');
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios(url).then((response) => {
+  const todo = response.data as Todo;
+  const id = todo.id;
+  const title = todo.title;
+
+  const finished = todo.completed;
+  logTodo(id, title, finished);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(
+    `the todo with id : ${id} 
+    has a title of ${title} 
+    and is it finished? ${completed}`
+  );
+};
