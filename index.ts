89e66191167267d3edffdf6b4e06cc3092770dd0
MCSeal <mzeal93@gmail.com>
2022-12-07 11:02:58 -0500

    going through object literals, already knew

--- index.ts
+++ index.ts
@@ -1,25 +1,55 @@
-import axios from 'axios';
-const url = 'https://jsonplaceholder.typicode.com/todos/1';
+// import axios from 'axios';
+// const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-}
+// interface Todo {
+//   id: number;
+//   title: string;
+//   completed: boolean;
+// }
 
-axios(url).then((response) => {
-  const todo = response.data as Todo;
-  const id = todo.id;
-  const title = todo.title;
+// axios(url).then((response) => {
+//   const todo = response.data as Todo;
+//   const id = todo.id;
+//   const title = todo.title;
 
-  const finished = todo.completed;
-  logTodo(id, title, finished);
-});
+//   const finished = todo.completed;
+//   logTodo(id, title, finished);
+// });
 
-const logTodo = (id: number, title: string, completed: boolean) => {
-  console.log(
-    `the todo with id : ${id} 
-    has a title of ${title} 
-    and is it finished? ${completed}`
-  );
+// const logTodo = (id: number, title: string, completed: boolean) => {
+//   console.log(
+//     `the todo with id : ${id}
+//     has a title of ${title}
+//     and is it finished? ${completed}`
+//   );
+// };
+
+// const today = new Date();
+
+// today.getMonth();
+
+// const person = {
+//   age: 25,
+
+// };
+
+// class color {
+
+// }
+
+// const red = new color();
+
+let speed: string = 'fast';
+let hasName: boolean = false;
+
+let apples: string[] = ['macintosh', 'fuji', 'granny smith'];
+let numbers: number[] = [1, 2, 3];
+
+class Car {}
+
+let car: Car = new Car();
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
 };
