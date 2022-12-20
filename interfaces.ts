b521d05fdd2bd74b9645991529ef486ab6008f31
MCSeal <mzeal93@gmail.com>
2022-12-20 13:48:02 -0500

    interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -2,12 +2,16 @@ interface car {
   name: string;
   year: Date;
   broken: boolean;
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'civic',
   year: new Date(),
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}, maybe it's red?`;
+  },
 };
 
 // const printVehicle = (vehicle:car) => {
@@ -29,9 +33,7 @@ const oldCivic = {
 // console.log(printVehicle(oldCivic));
 
 const printVehicle = (vehicle: car): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken?: ${vehicle.broken}`);
+  console.log(vehicle.summary());
 };
 
 console.log(printVehicle(oldCivic));
