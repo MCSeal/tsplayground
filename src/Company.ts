17e28d73df171ac89921698b7afadffb92bfb7b1
MCSeal <mzeal93@gmail.com>
2023-01-03 19:27:03 -0500

    added notes

--- src/Company.ts
+++ src/Company.ts
@@ -11,6 +11,7 @@ export class Company {
   constructor() {
     this.companyName = faker.company.name();
     this.catchPhrase = faker.company.catchPhrase();
+    
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
