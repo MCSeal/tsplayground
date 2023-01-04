d740848e4baebd1d289835b9cae21ba94993c19e
MCSeal <mzeal93@gmail.com>
2023-01-04 17:03:18 -0500

    added map

--- src/Company.ts
+++ src/Company.ts
@@ -11,10 +11,12 @@ export class Company {
   constructor() {
     this.companyName = faker.company.name();
     this.catchPhrase = faker.company.catchPhrase();
-    
+
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
     };
   }
 }
+
+//company stuff
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
+/// <reference types="@types/google.maps" />
 import { User } from './User';
 import { Company } from './Company';
 
 const user = new Company();
 
 console.log(user);
+
+console.log(google.maps);
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
